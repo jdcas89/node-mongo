58cf8b5f7550e54539b70ecdcfd26fbf6478183f
Juan David Castellanos <juandavid.castellanos@live.com>
2017-10-04 06:50:59 +0200

    initial commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,37 @@
+const MongoClient = require('mongodb').MongoClient
+
+const dbName = 'TodoApp'
+const mongoUrl = `mongodb://localhost:27017/${dbName}`
+
+MongoClient.connect(mongoUrl, (err, db) => {
+  if (err){
+    return console.log('Unable to connect to MongoDB server')
+  }
+  console.log('Connected to MongoDB server')
+
+  db.collection('Todos').insertOne({
+    text: 'A note',
+    completed: false
+  }, (err, result) => {
+    if (err){
+      return console.log('Unable to insert todo', err)
+    }
+
+    console.log(JSON.stringify(result.ops, undefined, 2))
+  })
+
+
+  db.collection('Users').insertOne({
+    name: 'JD',
+    age: 66,
+    location: 'World'
+  }, (err, result) => {
+    if (err){
+      return console.log('Unable to insert user', err)
+    }
+
+    console.log(JSON.stringify(result.ops, undefined, 2))
+  })
+
+  db.close();
+})
