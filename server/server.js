43607c33ece00acaedad608436614b28f1ce348c
Juan David Castellanos <juandavid.castellanos@live.com>
2017-10-07 12:45:25 +0200

    Added Get /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,36 +1,68 @@
 var express = require('express')
 var bodyParser = require('body-parser')
-
-var {mongoose} = require('./db/mongoose')
-var {Todo} = require('./models/todo')
-var {User} = require('./models/user')
+const {
+  ObjectID
+} = require('mongodb')
+var {
+  mongoose
+} = require('./db/mongoose')
+var {
+  Todo
+} = require('./models/todo')
+var {
+  User
+} = require('./models/user')
 
 var app = express()
 app.use(bodyParser.json())
 
-app.post('/todos', (req,res) => {
+app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
   })
 
-  todo.save().then((doc) =>{
+  todo.save().then((doc) => {
     res.send(doc)
-  }, (err) =>{
+  }, (err) => {
     res.status(400).send(err)
   })
 })
 
 app.get('/todos', (req, res) => {
-  
-  Todo.find().then((todos) => {
-    res.send({todos})
-  }, (err) => {
-      res.status(400).send(err)
-    });
-  })
 
-app.listen(3000, () =>{
+  Todo.find().then((todos) => {
+    res.send({
+      todos
+    })
+  }, (err) => {
+    res.status(400).send(err)
+  });
+})
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
+
+
+  Todo.findById(id).then((todo) => {
+    if(!todo){
+      return res.status(404).send({})
+    }
+    res.send({
+      todo
+    })
+  }, (err) => {
+    res.status(400).send()
+  });
+})
+
+
+app.listen(3000, () => {
   console.log('Started on port 3000')
 })
 
-module.exports = {app}
+module.exports = {
+  app
+}
