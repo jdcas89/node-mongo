da1243ee8be69a98f5a7358ccefb38ca8fa2e8d1
Juan David Castellanos <juandavid.castellanos@live.com>
2017-10-10 21:44:35 +0200

    mongoose remove

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,21 @@
+const { mongoose } = require('./../server/db/mongoose')
+const {Todo } = require('./../server/models/todo')
+const {User} = require('./../server/models/user')
+const {ObjectID} = require('mongodb')
+
+
+//remove all documents
+// Todo.remove({}).then((result) => {
+//   console.log(result)
+// })
+
+
+//remove 1 with specific query
+// Todo.findOneAndRemove({}).then((todo) => {
+//   console.log(todo)
+// })
+
+//find by id and remove
+Todo.findByIdAndRemove({}).then((todo) => {
+  console.log(todo)
+})
