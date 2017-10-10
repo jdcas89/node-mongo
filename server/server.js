ca25a60d3dc0a831da04e6ccf0c2422cd765ac93
Juan David Castellanos <juandavid.castellanos@live.com>
2017-10-10 21:14:56 +0200

    added delete route

--- server/server.js
+++ server/server.js
@@ -61,6 +61,28 @@ app.get('/todos/:id', (req, res) => {
 })
 
 
+app.delete('/todos/:id', (req, res) => {
+  const id = req.params.id
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
+
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if(!todo){
+      return res.status(404).send({})
+    }
+    res.send({
+      todo
+    })
+  }, (err) => {
+    res.status(400).send()
+  });
+})
+
+
+
+
 app.listen(port, () => {
   console.log(`Started server on port ${ port }`)
 })
