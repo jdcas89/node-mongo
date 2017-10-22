bc79a23557135a5d6d760ad1d551f416f844baf2
dev <dev@admins-iMac.local>
2017-10-22 17:33:29 +0200

    Added Delete /users/me/token tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -332,3 +332,22 @@ describe('Post /users/login', () => {
 
   })
 })
+
+describe('DELETE /users/me/token', () => {
+  it('Should remove auth token on logout', (done) => {
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .end((err, res) =>{
+        if(err){
+          return done(err)
+        }
+  
+        User.findById(users[0]._id).then((user) => {
+          expect(user.tokens.length).toBe(0)
+          done()
+        }).catch((e) => done(e))
+      })
+  })
+})
